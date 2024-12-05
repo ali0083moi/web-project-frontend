4308b57e03cafe7880838f736b62abe28f65dd5f
ali0083moi <ali0083moghadasi.mypc@gmail.com>
2024-12-05 22:29:38 +0330

    change the footer and about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -77,7 +77,7 @@ export default function AboutUs() {
       </div>
 
       {/* Technologies Section */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-bold text-white text-center mb-12">
           تکنولوژی‌های مورد استفاده
         </h2>
