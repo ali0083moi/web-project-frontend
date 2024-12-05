abc3c6772da39a45284b60878e8c1b2b639ed194
ali0083moi <ali0083moghadasi.mypc@gmail.com>
2024-12-05 22:05:49 +0330

    Add AuthContext for user authentication management
    - Implemented AuthContext to manage user authentication state.
    - Added login and logout functions with API integration for user sessions.
    - Utilized localStorage to persist user data across sessions.
    - Created a custom hook, useAuth, for easy access to authentication context.
    - Integrated routing to redirect users based on their roles after login.

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { createContext, useState, useEffect, useContext } from "react";
+import { useRouter } from "next/navigation";
+import { User } from "@/src/models/User";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check for stored user data on initial load
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (res.ok) {
+        const userData: User = await res.json();
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+        router.push(userData.role === "designer" ? "/designer" : "/player");
+      } else {
+        throw new Error("Login failed");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      throw error;
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    router.push("/login");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated: !!user, user, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
